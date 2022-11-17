a8f100b03fc4ad0ff7a1235f9be4c4b9bd04bac6
Jevenden <102879070+Jevenden@users.noreply.github.com>
2022-11-17 17:07:48 -0500

    service text, structure, and icons

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import How from "../img/how.png"
+import MiniCard from './MiniCard'
+import Play from "../img/play.png"
 
 const Container = styled.div`
     display: flex;
@@ -34,6 +36,31 @@ margin-top: 20px;
 color: #555;
 `
 
+const CardContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 50px;
+`
+
+const Button = styled.button`
+    width: 180px;
+    border: none;
+    border-radius: 10px;
+    background-color: darkblue;
+    color: white;
+    font-size: 20px;
+    padding: 15px;
+    margin-top: 50px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+`
+
+const Icon = styled.img`
+    width: 20px;
+    margin-right: 10px;
+`
+
 const Service = () => {
   return (
     <Container>
@@ -41,9 +68,16 @@ const Service = () => {
             <Image src={How}/>
         </Left>
         <Right>
-            <Wrapper />
+            <Wrapper>
             <Title>Simple processes to start</Title>
             <Desc>Our platform provides a simple way to start your business. You can create your own website and start selling your products online.</Desc>
+        <CardContainer>
+            <MiniCard/>
+            <MiniCard/>
+            <MiniCard/>
+        </CardContainer> 
+        <Button><Icon src={Play}/>Learn More</Button>
+        </Wrapper>
         </Right>
     </Container>
   )
